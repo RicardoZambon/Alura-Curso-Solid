0d23169b7a9c5d797f9a9ed6e639c6ef7d45401e
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-22 17:23:48 -0300

    refactor: change controller from AppDbContext to DAO's
    Change LeiloesController to use LeiloesDAO and CategoriasDAO instead of the AppDbContext itself.
    
    #5

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/BaseDAO.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public abstract class BaseDAO<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseDAO(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public virtual IQueryable<T> Listar()
+            => _context.Set<T>();
+
+        public virtual T ProcurarPorId(int id)
+            => _context.Set<T>().Find(id);
+
+
+        public virtual void Inserir(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual void Alterar(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual void Remover(T leilao)
+        {
+            _context.Set<T>().Remove(leilao);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ src/Alura.LeilaoOnline.WebApp/Dados/CategoriasDAO.cs
@@ -0,0 +1,11 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class CategoriasDAO : BaseDAO<Categoria>
+    {
+        public CategoriasDAO(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
