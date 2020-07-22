b0ebaac28cd7b1db41f646dbdcef96d552b53ed9
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-22 19:06:10 -0300

    refactor: removed strong dependency from ef core in HomeController
    Remove strong depencency in HomeControlle from EFCore, moved to DAO classes.

--- src/Alura.LeilaoOnline.WebApp/Dados/DAO/EfCore/CategoriasDao.cs
+++ src/Alura.LeilaoOnline.WebApp/Dados/DAO/EfCore/CategoriasDao.cs
@@ -1,4 +1,7 @@
 ﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Alura.LeilaoOnline.WebApp.Dados.DAO.EfCore
 {
@@ -7,5 +10,26 @@ namespace Alura.LeilaoOnline.WebApp.Dados.DAO.EfCore
         public CategoriasDao(AppDbContext context) : base(context)
         {
         }
+
+
+        public override IQueryable<Categoria> List()
+            => base.List()
+                .Include(c => c.Leiloes);
+
+        public IQueryable<CategoriaComInfoLeilao> ListWithAuction()
+            => List()
+                .Select(c => new CategoriaComInfoLeilao
+                {
+                    Id = c.Id,
+                    Descricao = c.Descricao,
+                    Imagem = c.Imagem,
+                    EmRascunho = c.Leiloes.Where(l => l.Situacao == SituacaoLeilao.Rascunho).Count(),
+                    EmPregao = c.Leiloes.Where(l => l.Situacao == SituacaoLeilao.Pregao).Count(),
+                    Finalizados = c.Leiloes.Where(l => l.Situacao == SituacaoLeilao.Finalizado).Count(),
+                });
+
+
+        public override Categoria FindByID(int id)
+            => _context.Set<Categoria>().Include(x => x.Leiloes).First(x => x.Id == id);
     }
 }
